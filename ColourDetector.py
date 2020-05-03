ce31fc01b8cc4af6f864b67d1debbf23f062bd41
Simon Ditner <spditner@gmail.com>
2020-05-03 12:38:04 -0400

    Switch to L*a*b*

--- ColourDetector.py
+++ ColourDetector.py
@@ -6,6 +6,9 @@ import sys
 
 class ColourDetector:
     def __init__(self, colors=None):
+
+        self.lab = np.zeros((len(colors), 1, 3), dtype="uint8")
+        self.colorNames = []
         if not colors:
             colors = OrderedDict({
                 # Use "Digital Color Meter" app to help you find colours on MacOS
@@ -15,9 +18,6 @@ class ColourDetector:
                 "violet": (140, 60, 140),
             })
 
-        self.lab = np.zeros((len(colors), 1, 3), dtype="uint8")
-        self.colorNames = []
-
         for (i, (name, rgb)) in enumerate(colors.items()):
             self.lab[i] = rgb
             self.colorNames.append(name)
@@ -25,7 +25,7 @@ class ColourDetector:
         # ΔL* (L* sample minus L* standard) = difference in lightness and darkness (+ = lighter, – = darker)
         # Δa* (a* sample minus a* standard) = difference in red and green (+ = redder, – = greener)
         # Δb* (b* sample minus b* standard) = difference in yellow and blue (+ = yellower, – = bluer)
-        # self.lab = cv2.cvtColor(self.lab, cv2.COLOR_RGB2LAB)
+        self.lab = cv2.cvtColor(self.lab, cv2.COLOR_RGB2LAB)
 
     def label(self, image, c):
         mask = np.zeros(image.shape[:2], dtype="uint8")
@@ -42,12 +42,23 @@ class ColourDetector:
         return self.colorNames[minDist[1]]
 
     def find(self, color):
+        #import pdb; pdb.set_trace()
+        lcolor = np.zeros((1, 1, 3), dtype="uint8")
+        lcolor[0] = color
+        # Seems to want it in some funny array shape
+        color = cv2.cvtColor(lcolor, cv2.COLOR_RGB2LAB)[0][0]
         minDist = (np.inf, None)
         for (i, row) in enumerate(self.lab):
             d = dist.euclidean(row[0], color)
             if d < minDist[0]:
                 minDist = (d, i)
-        sys.stderr.write("minDist: %r\n" % minDist[0])
-        if minDist[0] > 30:
+        sys.stderr.write(
+            "minDist: %r / %r / %r\n" % (
+                minDist[0],
+                tuple(color),
+                tuple(self.lab[minDist[1]][0]),
+            )
+        )
+        if minDist[0] > 80:
             return None
         return self.colorNames[minDist[1]]
--- kmeans.py
+++ kmeans.py
@@ -1,3 +1,4 @@
+import glob
 import sys
 from collections import OrderedDict
 
@@ -6,6 +7,9 @@ import matplotlib.pyplot as plt
 import argparse
 import utils
 import cv2
+
+# from ShapeDetector import ShapeDetector
+from ColourDetector import ColourDetector
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
@@ -16,10 +20,10 @@ ap.add_argument("-c", "--clusters", required=True, type=int, help="# of clusters
 ap.add_argument("-s", "--show", required=False, type=bool, help="Show result", default=False)
 args = vars(ap.parse_args())
 
-def kmeans(image, clusters, calibrate=None):
+def kmeans(image, clusters):
     # load the image and convert it from BGR to RGB so that
     # we can dispaly it with matplotlib
-    #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     # convert to LAB format 
     #image = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
 
@@ -58,16 +62,6 @@ def kmeans(image, clusters, calibrate=None):
         s_hist.append(hist[idx])
         s_clusters.append(clt.cluster_centers_[idx])
 
-    # Probably a card... If a calibration colour provided, try to match it
-    # RESULT: Did not improve the situation
-    if len(s_hist) == (clusters-1) and len(s_discarded) and calibrate is not None:
-        adj = [None] * len(calibrate)
-        for idx, color in enumerate(calibrate):
-            adj[idx] = color / s_discarded[idx]
-        for idx, _ in enumerate(s_clusters):
-            s_clusters[idx] *= adj[idx]
-        sys.stderr.write(">>> Calibrate %s" % adj)
-
     # Rescale
     if discarded:
         for i, _ in enumerate(s_hist):
@@ -81,11 +75,25 @@ def kmeans(image, clusters, calibrate=None):
         plt.axis("off")
         plt.imshow(bar)
         plt.show()
+
+    # Returns: [(percentage, RGB), ...], top discarded RGB
     return OrderedDict(zip(s_hist, s_clusters)), s_discarded
 
-import glob
-from ColourDetector import ColourDetector
-def compare_many(src, compare_dir, clusters):
+def plot_bar(hist, clusters, hist2, clusters2):
+    bar = [
+        utils.plot_colors(hist, clusters),
+        utils.plot_colors(hist2, clusters2),
+    ]
+    fig = plt.figure(figsize=(4, 4))
+    plt.axis("off")
+    for i in range(2):
+        fig.add_subplot(2, 2, i+1)
+        plt.axis("off")
+        plt.imshow(bar[i])
+    plt.show()
+
+def compare_many(src, compare_dir, clusters, match_data):
+    result = open("result.txt", "w")
     image = cv2.imread(src)
     km1, calibrate = kmeans(image, clusters)
     detector = ColourDetector(colors=km1)
@@ -96,32 +104,44 @@ def compare_many(src, compare_dir, clusters):
         image2 = cv2.imread(filename)
         same_colour = False
         try:
-            km2, _ = kmeans(image2, clusters) #, calibrate)
+            km2, _ = kmeans(image2, clusters)
         except ValueError:
             sys.stderr.write("Not enough colours")
         else:
             same_colour = any([detector.find(c) for c in km2.values()])
 
-        sys.stderr.write("%s\n%s\n%s\n" % (km1, km2, same_colour))
-        sys.stdout.write("%s:%d\n" % (filename, not same_colour))
+#        sys.stderr.write("%s\n%s\n%s\n" % (km1, km2, same_colour))
+        result.write("%s:%d\n" % (filename, not same_colour))
 
+        if match_data[filename] == same_colour:
+            print("TRUE")
+        else:
+            print("FALSE: Expected %s for %s, Found: %s" % (
+                match_data[filename],
+                filename,
+                same_colour,
+            ))
+#            plot_bar(
+#                km1.keys(), km1.values(),
+#                km2.keys(), km2.values(),
+#            )
+        print("---")
     sys.stderr.write("%s\n" % calibrate)
 
+def read_expected(filename="expected.txt"):
+    match = {}
+    with open(filename) as fh:
+        for line in fh.readlines():
+            path, value = line.strip().split(":")
+            value = True if value == "0" else False
+            match[path] = value
+    return match
+
 if __name__ == "__main__":
+    match_data = read_expected()
     compare_many(
         args["image"],
         args["image2"],
-        args["clusters"]
+        args["clusters"],
+        match_data,
     )
-
-
-##same_colour = True
-##for pct, colour in km2.items():
-##    matched_pct = detector.find(colour)
-##    if not matched_pct:
-##        same_colour = False
-#same_colour = any([detector.find(c) for c in km2.values()])
-#print("%s\n%s\n%s" % (km1, km2, same_colour))
-#if same_colour:
-#    sys.exit(0)
-#sys.exit(-1)
